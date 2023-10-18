d3275f3570e9c839728300a946974d8e61262357
Teixeira <vteixeira212@gmail.com>
2023-10-18 20:15:48 -0300

    chore: configure boostrap

--- src/App.js
+++ src/App.js
@@ -1,79 +1,86 @@
-import './App.css';
-
 import { books } from "./Api";
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 
-// eslint-disable-next-line import/no-anonymous-default-export
-export default () => { 
-  const [bookList, setBookList] = useState([]);
+import { Button, Form, CloseButton, ListGroup, Badge, Container, Row } from 'react-bootstrap';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const BookElement = ({ data, callback }) => {
+  async function remove(id) {
+    await books.delete(id);
+    callback();
+  }
+
+  return (
+    <ListGroup>
+      {data.map((data) => (
+        <ListGroup.Item as="li" className="d-flex justify-content-between align-items-start">
+          <div className="ms-2 me-auto">
+            <div className="fw-bold">{data.name}</div>
+          </div>
+          <CloseButton style={{ float: 'right' }} onClick={() => { remove(data.id) }}></CloseButton >
+        </ListGroup.Item>
+      ))}
+    </ListGroup>
+  )
+}
+
+const FormCreateBook = ({ callback }) => {
   const [book, setBook] = useState({
-    name : "Livro 1",
+    id: "",
+    name: "Livro 1",
     description: "Descricao ",
-    medium_price : 0,
+    medium_price: 0,
     img_url: ""
   });
 
-  function updateBook(obj){
-    console.log(obj)
-    setBook(obj)
+  async function createBook() {
+    await books.create(book);
+    callback()
   }
+
+  return (
+    <Form>
+      <Form.Group className="mb-3" controlId="formNameBook">
+        <Form.Label htmlFor="name_book">Nome do livro:</Form.Label>
+        <Form.Control type="text" placeholder="Digite o nome do Livro" value={book.name || ''} onChange={(evet) => { setBook(prevState => ({ ...prevState, name: evet.target.value })) }} />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formDescriptionBook">
+        <Form.Label htmlFor="description_book">Descricao do livro:</Form.Label>
+        <Form.Control as="textarea" placeholder="Digite uma breve descricao do livro" value={book.description || ''} onChange={(evet) => { setBook(prevState => ({ ...prevState, description: evet.target.value })) }} />
+      </Form.Group>
+      <Button variant="primary" onClick={createBook}>
+        Criar livro
+      </Button>
+    </Form >
+  )
+}
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default () => {
+  const [bookList, setBookList] = useState([]);
 
   async function fetchBooks() {
     const response = await books.index();
     setBookList(response)
   }
 
-
-  async function createBook() {
-    await books.create(book);
-    fetchBooks()
-  }
-
-  async function deleteBook(id) {
-    await books.delete(id);
-    fetchBooks()
-  }
+  useEffect(() => {
+    fetchBooks();
+  }, [])
 
   return (
-    <div className="App">
-        <p>
-          Ola Vinicius
-        </p>
-        <form>
-          <label htmlFor="name_book">Nome do livro:</label><br/>
-          <input value={book.name || ''} type='text' id="name_book" name="name_book" 
-          onChange={(evet) =>{ 
-              setBook(prevState => (
-                {                  
-                    ...prevState,
-                    name: evet.target.value
-                }
-            ))
-          }} ></input><br/>
-          <label htmlFor="description_book">Descricao do livro:</label><br/>
-          <input value={book.description || ''} type='text' id="description_book" name="description_book"  
-            onChange={(evet) =>{ 
-              setBook(prevState => (
-                {                  
-                    ...prevState,
-                    description: evet.target.value
-                }
-            ))}}
-          ></input><br/>
-        </form>
-
-        <button onClick={createBook}>
-          Criar livro
-        </button>
-
-        {bookList.map((data) => (
-            <p key={data.id}>
-              
-              {data.name}
-
-              <button onClick={() => { deleteBook(data.id)}}>X</button>
-            </p>
-        ))}
-    </div>
+    <Container>
+      <Row>
+        <Button onClick={fetchBooks}> Atualizar</Button>
+      </Row>
+      <Row>
+        <FormCreateBook callback={fetchBooks}></FormCreateBook>
+      </Row>
+      <Row>
+        <BookElement data={bookList} callback={fetchBooks}></BookElement>
+      </Row>
+    </Container>
   );
 }
